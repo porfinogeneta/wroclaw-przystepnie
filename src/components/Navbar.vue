d3c213970905d11fcdf2a7eb70113100a89354b2
porfinogeneta <szyman123drum@gmail.com>
2021-01-17 12:03:10 +0100

    test

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="height: 10vh"
       class="sticky py-3 m-0 text-4xl bg-gradient-to-r from-gray-800 to-gray-600 w-full text-blue-50 z-99">
-    <h1 class="text-center mx-2">Wrocław Przystępnie</h1>
+    <h1 class="text-center mx-2">Wrocław Przystępnie - poznaj lepiej stolicę Dolnego Śląska</h1>
     <section class="py-5 mx-4" v-for="link in sites" :key="link">
 <!--      <router-link :to=link.to>{{ link.name }}</router-link>-->
     </section>
