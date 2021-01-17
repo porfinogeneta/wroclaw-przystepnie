6bb0519356c41c1baba76b4081a2fd26ef8224d7
porfinogeneta <szyman123drum@gmail.com>
2021-01-17 11:24:00 +0100

    almost done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   purge: ["./pages/**/*.vue",
-    "./components/**/*.vue",
-    "./plugins/**/*.vue",
-    "./static/**/*.vue",
-    "./store/**/*.vue"],
+    "./components/**/*.vue",],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {translate: ['motion-safe'],
