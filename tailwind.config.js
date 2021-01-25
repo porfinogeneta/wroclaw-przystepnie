d028c980ffc8587ed170340a0eaa9c32a711bdf5
porfinogeneta <szyman123drum@gmail.com>
2021-01-25 08:12:40 +0100

    css changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: [
+        './pages/About.vue',
+        './pages/Home.vue'
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {translate: ['motion-safe'],
